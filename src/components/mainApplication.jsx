1a853a826a547b4ff1e4d03384a82943a70084de
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-04-18 13:09:54 +0300

    recomposition and start writing page

---
+++ src/components/mainApplication.jsx
@@ -0,0 +1,9 @@
+import Header from './header.jsx'
+
+const MainApplication = () => {
+    return (
+        <Header/>
+    )
+    }   
+
+export default MainApplication
---
+++ src/styles/index.js
@@ -0,0 +1,8 @@
+import './normalize.css'
+import './global.css'
+import './fonts.css'
+import './utils.css'
+import './global.css'
+
+
+import './components/button.css'
