63bb0a4ebe261f0e7ec934c027a3d239f55524f6
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-04-21 23:25:48 +0300

    update porject

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from 'react'
 import LottiePackage from "lottie-react";
 import animationData from "../assets/animations/programming.json";
 
 const Lottie = LottiePackage.default || LottiePackage;
+
+
+const TypingText = ({ text, speed = 100 }) => {
+    const [displayedText, setDisplayedText] = useState('');
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        if (index < text.length) {
+        const timeout = setTimeout(() => {
+            setDisplayedText((prev) => prev + text[index]);
+            setIndex(index + 1);
+        }, speed);
+            return () => clearTimeout(timeout);
+        }
+    }, [index, text, speed]);
+
+        return <span>{displayedText}</span>;
+    };
+
+// Использование:
+
 
 const MainContent = () => {
     return (
@@ -11,6 +33,7 @@ const MainContent = () => {
                 <p>I am a Frontend Developer</p>
             <p>I can do some great things with your website</p>
         </div>
+
         {typeof Lottie === 'function' || typeof Lottie === 'object' ? (
             <Lottie animationData={animationData} loop={true} />)
                 : ( <p>Ошибка загрузки Lottie</p>)}
