7cfc673d0e70a12de0c2859744045956086e6f44
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-04-27 22:38:39 +0300

    update project, fixed main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Header from './components/header';
 import MainContent from './components/MainContent';
 import MainApplication from './components/MainApplication';
-import TypingText from './components/MainContent'
 
 function App() {
   return (
     <MainApplication>
       <Header />
-      <TypingText text="Этот текст печатается..." speed={20} />
       <MainContent />
     </MainApplication>
   );
