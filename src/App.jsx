9c19dad67d0735ccc33d5abadca55299c34368c8
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-05-05 23:11:51 +0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MainApplication from './components/MainApplication';
 import Skills from './components/Skills';
 import ButtonCheck from './components/ButtonCheck';
 import MyPortfolio from './components/MyPortfolio';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <ButtonCheck/>
       <Skills/>
       <MyPortfolio/>
+      <Footer/>
     </MainApplication>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer-box-text">
+                <p className="footer-text">Do you want to ask?</p>
+                <p className="footer-description">Contact with me. I'm in touch mon-fri from 8 am to 8 pm (gmt).</p>
+            </div>
+            <a className="footer-link"><img src></img></a>
+            <a className="footer-link"><img src></img></a>
+            <a className="footer-link"><img src></img></a>
+            <a className="footer-link"><img src></img></a>
+            <a className="footer-link"><img src></img></a>
+            
+        </div>
+    )
+}
+
+export default Footer
