281b6e6ad171065bdd6c5b42c5d7443562f5bd29
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-05-06 23:00:01 +0300

    update project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,13 @@ const Footer = () => {
                 <p className="footer-text">Do you want to ask?</p>
                 <p className="footer-description">Contact with me. I'm in touch mon-fri from 8 am to 8 pm (gmt).</p>
             </div>
-            <a className="footer-link"><img src></img></a>
-            <a className="footer-link"><img src></img></a>
-            <a className="footer-link"><img src></img></a>
-            <a className="footer-link"><img src></img></a>
-            <a className="footer-link"><img src></img></a>
-            
+            <div className="footer-box-link">
+                <a className="footer-link"><img src></img></a>
+                <a className="footer-link"><img src></img></a>
+                <a className="footer-link"><img src></img></a>
+                <a className="footer-link"><img src></img></a>
+                <a className="footer-link"><img src></img></a>
+            </div>
         </div>
     )
 }
