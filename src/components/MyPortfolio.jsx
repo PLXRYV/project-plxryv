46eada0b9d820407b17f1a9f08a61e883e0ed2f9
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-05-12 15:42:53 +0300

    update project

--- src/components/MyPortfolio.jsx
+++ src/components/MyPortfolio.jsx
@@ -11,13 +11,13 @@ const MyPortfolio = () => {
                     <p className="portfolio-item-description">Multi-page website, based on code with integration of several technologies:
                         Vite; React; CustomHooks; JavaScript; SCSS; HTML; Node.JS.
                     </p>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-css.png" alt="CSS"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-sass.png" alt="SASS"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-javascript.png" alt="JavaScript"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-react.png" alt="React"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-vite.png" alt="Vite"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-node-js.png" alt="Node.JS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-css.png" alt="CSS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-sass.png" alt="SASS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-javascript.png" alt="JavaScript"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-react.png" alt="React"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-vite.png" alt="Vite"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-node-js.png" alt="Node.JS"></img>
                 </div>
             </div>
             <div className="myportfolio-item">
@@ -27,11 +27,11 @@ const MyPortfolio = () => {
                     <a className="portfolio-item-link">ToDo-List</a>
                     <p className="portfolio-item-description">Simple ToDo-List application, where you can add, delete and edit your tasks. I created this project to practice my skills in JavaScript and React. I used at this project custom hooks, which I created myself, to make the code more organized and reusable.
                     </p>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-sass.png" alt="SASS"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-react.png" alt="JavaScript"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-node-js.png" alt="Node.JS"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-vite.png" alt="Vite"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-sass.png" alt="SASS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-react.png" alt="JavaScript"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-node-js.png" alt="Node.JS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-vite.png" alt="Vite"></img>
                 </div>
             </div>
             <div className="myportfolio-item">
@@ -41,11 +41,11 @@ const MyPortfolio = () => {
                     <a className="portfolio-item-link">Dance Studio Rakurs</a>
                     <p className="portfolio-item-description">Landing-page website, where I completely created the page, from design to launch on the host.
                     </p>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-css.png" alt="CSS"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-javascript.png" alt="JavaScript"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-node-js.png" alt="Node.JS"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-vite.png" alt="Vite"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-css.png" alt="CSS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-javascript.png" alt="JavaScript"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-node-js.png" alt="Node.JS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-vite.png" alt="Vite"></img>
                 </div>
             </div>
             <div className="myportfolio-item">
@@ -55,8 +55,8 @@ const MyPortfolio = () => {
                     <a className="portfolio-item-link">Spoon Landing</a>
                     <p className="portfolio-item-description">Landing-page website, where I completely created the page, from design to launch on the host.
                     </p>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
-                    <img className="portfolio-item-icon" src="/src/assets/icons/icons-css.png" alt="CSS"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-html.png" alt="HTML"></img>
+                    <img className="item-icon" src="/src/assets/icons/icons-css.png" alt="CSS"></img>
                 </div>
             </div>
             
@@ -64,5 +64,4 @@ const MyPortfolio = () => {
         
     )
 }
-
 export default MyPortfolio;
