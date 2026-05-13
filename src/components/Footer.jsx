50cf3b991639dfd9102e6cd14fb531f7f73b60e5
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-05-13 23:23:35 +0300

    update project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,20 @@
 const Footer = () => {
     return (
-        <div className="footer-container">
-            <div className="footer-box-text">
-                <p className="footer-text">Do you want to ask?</p>
-                <p className="footer-description">Contact with me. I'm in touch mon-fri from 8 am to 8 pm (gmt).</p>
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="footer-box-text">
+                    <p className="footer-text">Do you want to ask?</p>
+                    <p className="footer-description">Contact with me. I'm in touch mon-fri from 8 am to 8 pm (gmt).</p>
+                </div>
+                <div className="footer-box-link">
+                    <a><img className="link-icon" src="/src/assets/icons/icons-github.png" alt="GitHub"></img></a>
+                    <a><img className="link-icon" src="/src/assets/icons/icons-telegram.png"></img></a>
+                    <a><img className="link-icon" src="/src/assets/icons/icons-instagram.png"></img></a>
+                    <a><img className="link-icon" src="/src/assets/icons/icons-email.png"></img></a>
+                    <a><img className="link-icon" src="/src/assets/icons/icons-linkedin.png"></img></a>
+                </div>
             </div>
-            <div className="footer-box-link">
-                <a className="footer-link"><img src></img></a>
-                <a className="footer-link"><img src></img></a>
-                <a className="footer-link"><img src></img></a>
-                <a className="footer-link"><img src></img></a>
-                <a className="footer-link"><img src></img></a>
-            </div>
-        </div>
+        </footer>
     )
 }
 
