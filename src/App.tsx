76445b26bb088e71f5932660c1cd1c0ac90bbce3
Yaroslav Vorobev <yaroslavvorobev@MacBook-Air-Yaroslav.local>
2026-07-17 22:30:51 +0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Intro from './components/Intro.js';
-import Header from './components/header.js';
+import Header from './components/common/Header.js';
 import MainContent from './components/MainContent.js';
 import MainApplication from './components/MainApplication.js';
 import Skills from './components/Skills.js';
-import ButtonCheck from './components/ButtonCheck.js';
+import ButtonCheck from './ui/ButtonCheck.js';
 import MyPortfolio from './components/MyPortfolio.js';
-import Footer from './components/Footer.js';
+import Footer from './components/common/Footer.js';
 import WorkExperiance from './components/WorkExperiance.js';
 
 function App() {
